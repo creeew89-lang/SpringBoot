be6c1783902ccbbe88b7eebaa667ba0dcb24cdc0
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-11-24 10:39:50 +0400

    #TRA-6166: SpringBoot: Change your code to use database for data

---
+++ StudentdbApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.om.tra.codline.studentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
