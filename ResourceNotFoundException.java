e806e0062caae3add491e5caee62ff79060aae23
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-12-01 13:52:35 +0400

    #TRA-6192: InventoryApplication: Expections > ResourceNotFoundException

---
+++ ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package tra.codline.almuiayed.inventory.Exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException (String msg) {
+        super(msg);
+    }
+}
