cd12162f5e22f4b7d6f099b521039bd534740857
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-12-08 12:31:55 +0400

    Delete ResourceNotFoundException.java

--- ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package tra.codline.almuiayed.inventory.Exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException (String msg) {
-        super(msg);
-    }
-}
