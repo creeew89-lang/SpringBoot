10b4baf8af2d6446a52f242e3d8da9209fe05157
AL - Muiayed AL - Ansari <creeew89@gmail.com>
2025-12-08 12:25:05 +0400

    Delete apis/apis directory

--- apis/apis/src/main/java/com/codeline/apis/exception/DuplicateResourceException.java
+++
@@ -1,7 +0,0 @@
-package com.codeline.apis.exception;
-
-public class DuplicateResourceException extends RuntimeException{
-    public DuplicateResourceException(String msg){
-        super(msg);
-    }
-}
--- apis/apis/src/main/java/com/codeline/apis/exception/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.codeline.apis.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String msg){
-        super(msg);
-    }
-
-}
